bcaaab1419b5923b9020ad2aa379d2f25f1b3bdb
thanh <thanhrossi.tt@gmail.com>
2015-08-26 12:40:48 +0700

    update bar chart and pie chart

--- app.js
+++ app.js
@@ -22,3 +22,295 @@ var chart = new Chartist.Line('.hole-in-data', {
   },
   low: 0
 });
+
+// Only whole numbers
+new Chartist.Line('.only-whole-num', {
+  labels: [1, 2, 3, 4, 5, 6, 7, 8],
+  series: [
+    [1, 2, 3, 1, -2, 0, 1, 0],
+    [-2, -1, -2, -1, -3, -1, -2, -1],
+    [0, 0, 0, 1, 2, 3, 2, 1],
+    [3, 2, 1, 0.5, 1, 0, -1, -3]
+  ]
+}, {
+  high: 3,
+  low: -3,
+  fullWidth: true,
+  // As this is axis specific we need to tell Chartist to use whole numbers only on the concerned axis
+  axisY: {
+    onlyInteger: true,
+    offset: 20
+  }
+});
+// LINE tooltip
+new Chartist.Line('.line-tooltip', {
+  labels: ['1', '2', '3', '4', '5', '6'],
+  series: [
+    {
+      name: 'Fibonacci sequence',
+      data: [1, 2, 3, 5, 8, 13]
+    },
+    {
+      name: 'Golden section',
+      data: [1, 1.618, 2.618, 4.236, 6.854, 11.09]
+    }
+  ]
+});
+
+var $chart = $('.line-tooltip');
+
+var $toolTip = $chart
+  .append('<div class="tooltip"></div>')
+  .find('.tooltip')
+  .hide();
+
+$chart.on('mouseenter', '.ct-point', function() {
+  var $point = $(this),
+    value = $point.attr('ct:value'),
+    seriesName = $point.parent().attr('ct:series-name');
+  $toolTip.html(seriesName + '<br>' + value).show();
+});
+
+$chart.on('mouseleave', '.ct-point', function() {
+  $toolTip.hide();
+});
+
+$chart.on('mousemove', function(event) {
+  $toolTip.css({
+    left: (event.offsetX || event.originalEvent.layerX) - $toolTip.width() / 2 - 10,
+    top: (event.offsetY || event.originalEvent.layerY) - $toolTip.height() - 40
+  });
+});
+
+// Line area
+new Chartist.Line('.line-area', {
+  labels: [1, 2, 3, 4, 5, 6, 7, 8],
+  series: [
+    [5, 9, 7, 8, 5, 3, 5, 4]
+  ]
+}, {
+  low: 0,
+  showArea: true
+});
+
+// SVG animation 
+var chart = new Chartist.Line('.svg-animation', {
+  labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+  series: [
+    [1, 5, 2, 5, 4, 3],
+    [2, 3, 4, 8, 1, 2],
+    [5, 4, 3, 2, 1, 0.5]
+  ]
+}, {
+  low: 0,
+  showArea: true,
+  showPoint: false,
+  fullWidth: true
+});
+
+chart.on('draw', function(data) {
+  if(data.type === 'line' || data.type === 'area') {
+    data.element.animate({
+      d: {
+        begin: 2000 * data.index,
+        dur: 2000,
+        from: data.path.clone().scale(1, 0).translate(0, data.chartRect.height()).stringify(),
+        to: data.path.clone().stringify(),
+        easing: Chartist.Svg.Easing.easeOutQuint
+      }
+    });
+  }
+});
+// Bi polar bar chart
+var data = {
+  labels: ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7', 'W8', 'W9', 'W10'],
+  series: [
+    [1, 2, 4, 8, 6, -2, -1, -4, -6, -2]
+  ]
+};
+
+var options = {
+  high: 10,
+  low: -10,
+  axisX: {
+    labelInterpolationFnc: function(value, index) {
+      return index % 2 === 0 ? value : null;
+    }
+  }
+};
+
+new Chartist.Bar('.bi-polar-bar', data, options);
+
+// overlap-bar
+var data = {
+  labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+  series: [
+    [5, 4, 3, 7, 5, 10, 3, 4, 8, 10, 6, 8],
+    [3, 2, 9, 5, 4, 6, 4, 6, 7, 8, 7, 4]
+  ]
+};
+
+var options = {
+  seriesBarDistance: 10
+};
+
+var responsiveOptions = [
+  ['screen and (max-width: 640px)', {
+    seriesBarDistance: 5,
+    axisX: {
+      labelInterpolationFnc: function (value) {
+        return value[0];
+      }
+    }
+  }]
+];
+
+new Chartist.Bar('.overlap-bar', data, options, responsiveOptions);
+
+// stacked-bar
+new Chartist.Bar('.stacked-bar', {
+  labels: ['Q1', 'Q2', 'Q3', 'Q4'],
+  series: [
+    [800000, 1200000, 1400000, 1300000],
+    [200000, 400000, 500000, 300000],
+    [100000, 200000, 400000, 600000]
+  ]
+}, {
+  stackBars: true,
+  axisY: {
+    labelInterpolationFnc: function(value) {
+      return (value / 1000) + 'k';
+    }
+  }
+}).on('draw', function(data) {
+  if(data.type === 'bar') {
+    data.element.attr({
+      style: 'stroke-width: 30px'
+    });
+  }
+});
+
+// horizontal-bar
+new Chartist.Bar('.horizontal-bar', {
+  labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+  series: [
+    [5, 4, 3, 7, 5, 10, 3],
+    [3, 2, 9, 5, 4, 6, 4]
+  ]
+}, {
+  seriesBarDistance: 10,
+  reverseData: true,
+  horizontalBars: true,
+  axisY: {
+    offset: 70
+  }
+});
+
+// simple-pie-chart
+var data = {
+  series: [5, 3, 4]
+};
+
+var sum = function(a, b) { return a + b };
+
+new Chartist.Pie('.simple-pie-chart', data, {
+  labelInterpolationFnc: function(value) {
+    return Math.round(value / data.series.reduce(sum) * 100) + '%';
+  }
+});
+
+
+// pie-chart-custom
+var data = {
+  labels: ['Bananas', 'Apples', 'Grapes'],
+  series: [20, 15, 40]
+};
+
+var options = {
+  labelInterpolationFnc: function(value) {
+    return value[0]
+  }
+};
+
+var responsiveOptions = [
+  ['screen and (min-width: 640px)', {
+    chartPadding: 30,
+    labelOffset: 100,
+    labelDirection: 'explode',
+    labelInterpolationFnc: function(value) {
+      return value;
+    }
+  }],
+  ['screen and (min-width: 1024px)', {
+    labelOffset: 80,
+    chartPadding: 20
+  }]
+];
+
+new Chartist.Pie('.pie-chart-custom', data, options, responsiveOptions);
+// gauge-chart
+new Chartist.Pie('.gauge-chart', {
+  series: [20, 10, 30, 40]
+}, {
+  donut: true,
+  donutWidth: 60,
+  startAngle: 270,
+  total: 200,
+  showLabel: false
+});
+// animate-donut
+var chart = new Chartist.Pie('.animate-donut', {
+  series: [10, 20, 50, 20, 5, 50, 15],
+  labels: [1, 2, 3, 4, 5, 6, 7]
+}, {
+  donut: true,
+  showLabel: false
+});
+
+chart.on('draw', function(data) {
+  if(data.type === 'slice') {
+    // Get the total path length in order to use for dash array animation
+    var pathLength = data.element._node.getTotalLength();
+
+    // Set a dasharray that matches the path length as prerequisite to animate dashoffset
+    data.element.attr({
+      'stroke-dasharray': pathLength + 'px ' + pathLength + 'px'
+    });
+
+    // Create animation definition while also assigning an ID to the animation for later sync usage
+    var animationDefinition = {
+      'stroke-dashoffset': {
+        id: 'anim' + data.index,
+        dur: 1000,
+        from: -pathLength + 'px',
+        to:  '0px',
+        easing: Chartist.Svg.Easing.easeOutQuint,
+        // We need to use `fill: 'freeze'` otherwise our animation will fall back to initial (not visible)
+        fill: 'freeze'
+      }
+    };
+
+    // If this was not the first slice, we need to time the animation so that it uses the end sync event of the previous animation
+    if(data.index !== 0) {
+      animationDefinition['stroke-dashoffset'].begin = 'anim' + (data.index - 1) + '.end';
+    }
+
+    // We need to set an initial value before the animation starts as we are not in guided mode which would do that for us
+    data.element.attr({
+      'stroke-dashoffset': -pathLength + 'px'
+    });
+
+    // We can't use guided mode as the animations need to rely on setting begin manually
+    // See http://gionkunz.github.io/chartist-js/api-documentation.html#chartistsvg-function-animate
+    data.element.animate(animationDefinition, false);
+  }
+});
+
+// For the sake of the example we update the chart every time it's created with a delay of 8 seconds
+chart.on('created', function() {
+  if(window.__anim21278907124) {
+    clearTimeout(window.__anim21278907124);
+    window.__anim21278907124 = null;
+  }
+  window.__anim21278907124 = setTimeout(chart.update.bind(chart), 10000);
+});
